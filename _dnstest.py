9f8c72755685cd5a8a62ad58432b63a738edc524
anaiheer <anaiheer@gmail.com>
2026-08-17 00:46:32 +0800

    chore: remove temporary DNS diagnostic file

--- _dnstest.py
+++
@@ -1,6 +0,0 @@
-import socket
-for h in ('backend', 'postgres', 'redis'):
-    try:
-        print(h, '->', socket.getaddrinfo(h, 80)[0][4])
-    except Exception as e:
-        print(h, '-> ERR', e)
